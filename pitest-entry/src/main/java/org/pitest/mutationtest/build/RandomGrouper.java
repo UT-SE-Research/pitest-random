cf8e455686a1aea05b33787f327bc1c00b223f0e
Ljfanny <84827635+Ljfanny@users.noreply.github.com>
2025-01-09 01:33:15 -0600

    Delete useless libraries.

--- pitest-entry/src/main/java/org/pitest/mutationtest/build/RandomGrouper.java
+++ pitest-entry/src/main/java/org/pitest/mutationtest/build/RandomGrouper.java
@@ -9,7 +9,6 @@ import java.util.Collection;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 
 public class RandomGrouper implements MutationGrouper {
